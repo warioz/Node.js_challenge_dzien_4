7fee79fcebf208478f619c24f2d412377175c3b6
zaleckim <mar.zalecki@gmail.com>
2018-02-01 12:12:29 +0100

    Done

--- app/zadanieDnia2.js
+++ app/zadanieDnia2.js
@@ -1,1 +1,12 @@
-//Twój kod
+const http = require('http');
+
+const srv = http.createServer((req, res) => {
+    const userAgent = req.headers["user-agent"] || '';
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
+    res.write(userAgent);
+    res.end();
+});
+
+srv.listen(3000, () => {
+    console.log("Server start.");
+});
